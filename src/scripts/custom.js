68dd8e9534387a98210d4e6dd5139ce6089ea8cd
Kasper Laursen <Habberlabber@gmail.com>
2018-01-28 20:28:30 +0100

    Header animation added

--- src/scripts/custom.js
+++ src/scripts/custom.js
@@ -6,3 +6,10 @@
 | The main javascript file on the project
 |
 */
+
+$(document).ready(function(){
+
+  $('.animateIn').css('opacity', '0');
+  onViewport('header.animateIn', 'animated fadeInDown', 0);
+
+});
