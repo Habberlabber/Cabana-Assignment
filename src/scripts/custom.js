8210fd128e5850cc65d700b2dc60baa8f49dea4f
Kasper Laursen <Habberlabber@gmail.com>
2018-01-28 21:22:52 +0100

    Form validation and functionality added

--- src/scripts/custom.js
+++ src/scripts/custom.js
@@ -9,7 +9,51 @@
 
 $(document).ready(function(){
 
-  $('.animateIn').css('opacity', '0');
-  onViewport('header.animateIn', 'animated fadeInDown', 0);
+  /*
+  | Animations
+  */
+
+  $(".animateIn").css("opacity", "0");
+  onViewport("header.animateIn", "animated fadeInDown", 0);
+
+  /*
+  | Validation
+  */
+
+  $("#contact-form").validate({
+    errorClass: "is-invalid",
+    rules: {
+        name: {
+          minlength: 2
+      },
+        email: {
+          email: true
+      },
+        phone: {
+          minlength: 8
+      }
+    },
+    messages: {
+      firm: "Dette felt er påkrævet",
+      name: {
+        required: "Du skal udfylde dit navn",
+        minlength: "Aargh... Det er vist ikke dit navn. Prøv et andet"
+      },
+      email: {
+        required: "Du skal udfylde din mail, så vi kan kontakte dig",
+        email: "Den indtastede information er ikke en valid email"
+      },
+      phone: {
+        required: "Dette felt er påkrævet",
+        minlength: "Dit telefon nummer skal mindste være 8 tegn"
+      }
+    },
+    submitHandler: function(form) {
+      $("#contact-form").addClass("animated bounceOut");
+      setTimeout(function(){
+        $(".contact-alert").addClass("animated fadeIn");
+      }, 600);
+    }
+  });
 
 });
